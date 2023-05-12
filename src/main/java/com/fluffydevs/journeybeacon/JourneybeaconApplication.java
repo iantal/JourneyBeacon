ebb15fed3ae85859ed679509d366b2ee69243eaf
iantal <antal.micky@yahoo.com>
2023-05-12 22:19:34 +0300

    Spring Boot seed

---
+++ src/main/java/com/fluffydevs/journeybeacon/JourneybeaconApplication.java
@@ -0,0 +1,13 @@
+package com.fluffydevs.journeybeacon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneybeaconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneybeaconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fluffydevs/journeybeacon/JourneybeaconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluffydevs.journeybeacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneybeaconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
